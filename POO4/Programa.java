813056451008c046915ff62f52c2bdc38c68d827
Ândria Stone <andriastone37@gmail.com>
2024-09-03 10:56:01 -0400

    Encapsulamento

---
+++ POO4/Programa.java
@@ -0,0 +1,12 @@
+package POO4;
+
+public class Programa {
+    public static void main(String[] args) {
+        
+
+    ControleRemoto c = new ControleRemoto();
+
+    c.ligar();
+    c.abrirMenu();
+    }
+}
