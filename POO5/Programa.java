cdbac354645cb5a1dbcf5a0c5836b2456194f8df
Ândria Stone <andriastone37@gmail.com>
2024-09-08 16:21:33 -0400

    Corrigido

---
+++ POO5/Programa.java
@@ -0,0 +1,7 @@
+public class Programa {
+    public static void main(String[] args) {
+        
+        Lutador l = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, "Cateregoria", 11,2,1);
+        l.status();
+    }
+}
