97a2b54b89ca2235eefb942ff546c783655acc48
Ândria Stone <andriastone37@gmail.com>
2024-09-19 15:11:32 -0400

    Concuído o Polimorfismo

---
+++ POO10/Mamifero.java
@@ -0,0 +1,10 @@
+package POO10;
+
+public abstract class Mamifero {
+
+    protected double peso;
+    protected int membros;
+    protected int idade;
+
+    public abstract void emitirSom();
+}
---
+++ POO10/Programa.java
@@ -0,0 +1,11 @@
+package POO10;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        
+        Cachorro c = new Cachorro();
+
+        c.reagir("Olá");
+    }
+}
